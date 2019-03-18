a031b90469c95507de0e61cbc7a293478d29322e
hornie <hornminer@gmail.com>
2019-03-18 16:57:53 +0300

    no chainradar + favicon

--- website_example/config.js
+++ website_example/config.js
@@ -2,12 +2,12 @@ var api = "http://82.146.56.17:8117";
 
 var email = "pool@hleb.cc";
 var telegram = "https://t.me/hlebcoin_news";
-var discord = "https://discordapp.com/invite/YourPool";
+var discord = "https://discord.gg/cev66Px";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://explorer.hleb.cc/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.hleb.cc/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
